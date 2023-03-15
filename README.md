cfa07adc9df86b798af1e6288b17b27057162d8c
razan-aboushi <103279516+razan-aboushi@users.noreply.github.com>
2023-03-15 16:58:38 -0700

    added readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Caffe-Z
+### I made a website for a coffee house.
+
+### The live of my site [Black Coffee House ](https://razan-aboushi.github.io/Caffe-Z/).
+
+### I'm using the following tools :
+* VS code.
+* HTML.
+* CSS.
+* Bootstrap.
+* Javascript.
+* Git and Github.
---
+++ app.js
@@ -0,0 +1,30 @@
+let name =prompt("Please ,Enter your name");
+
+
+let gender =prompt("Please ,Enter your gender");
+
+if(gender=="female")
+{
+    alert("Hello" + " "+" Ms. "+ name +" " +"How was your day?");
+}
+else if(gender=="male")
+{
+    alert("Hello"+ " "+" Mr. "+ name +" " +"How was your day?" );
+}
+else 
+{
+    console.log("Welcome back!");
+}
+
+
+let drink =prompt("What do you want to drink , hot or cold drink?");
+
+let drinkname =prompt("Please enter the name of the drink you would like to drink");
+
+alert("Your drink is getting prepared");
+
+
+
+console.log(name +" " + drinkname);
+
+
