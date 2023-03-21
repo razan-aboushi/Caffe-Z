a5759337cfb26e79a0bfe7f17fee6a825359f80c
razan-aboushi <103279516+razan-aboushi@users.noreply.github.com>
2023-03-21 15:36:42 -0700

    added new branch

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let name = prompt("Please ,Enter your name");
 
+let gender = prompt("Please ,Enter your gender");
+
 function genderof()
 {
-let gender = prompt("Please ,Enter your gender");
 
 while (gender != "female" && gender != "male") {
     gender = prompt("Please ,Enter your gender");
@@ -18,6 +19,9 @@ else if (gender == "male") {
 }
 
 genderof();
+
+
+let age = prompt("Please ,Enter your Age");
 
 
 let drinktype = prompt("What do you want to drink , hot or cold drink?");
@@ -40,3 +44,49 @@ for (let R = 0; R <= 3; R++) {
 }
 
 console.log(`Your name is ${name} and your gender is ${gender} , you order ${drinktype} ${drinkname}`);
+
+
+
+
+
+
+
+const div1 = document.createElement("div");
+let container = document.getElementById("menu12");
+
+container.appendChild(div1);
+
+
+
+
+const par = document.createElement("p");
+
+let parname =document.createTextNode(`Your name is : ${name}`);
+
+div1.appendChild(par);
+par.appendChild(parname);
+
+
+const unlist = document.createElement("ul");
+par.appendChild(unlist);
+
+
+
+const li1 = document.createElement("li");
+let ligender =document.createTextNode(`Your gender is : ${gender}`);
+unlist.appendChild(li1);
+li1.appendChild(ligender);
+
+
+const li2 = document.createElement("li");
+let liAge =document.createTextNode(`Your age is : ${age}`);
+unlist.appendChild(li2);
+li2.appendChild(liAge);
+
+
+const li3 = document.createElement("li");
+let lidrink =document.createTextNode(`Your drink is ${drinktype} ${drinkname}`);
+unlist.appendChild(li3);
+li3.appendChild(lidrink);
+
+
